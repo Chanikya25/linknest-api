1f23c3290d1e3e4115e98c7e6a80b9174d0abbed
Chanikya25 <alluchanikya25@gmail.com>
2025-09-10 13:08:34 +0530

    feat: configure project for hosting

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,17 +1,10 @@
-// In src/db/index.ts
-import mysql from 'mysql2/promise';
+import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
